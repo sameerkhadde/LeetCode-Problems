71c8ce935bd53bec644cccc40425369c8ea629e9
Sameer Khadde <sameerkhadde.iet@srinivasuniversity.edu.in>
2026-06-10 09:32:49 +0530

    Time: 4 ms (42.44%) | Memory: 12.4 MB (69.24%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+
+        mapping = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        for char in s:
+            if char in mapping:
+                if not stack or stack.pop() != mapping[char]:
+                    return False
+            else:
+                stack.append(char)
+
+        return len(stack) == 0
